916d144190f7bfeee25483b827c06d836b9219a8
Shingirai Bhengesa <shingiebhengesa@gmail.com>
2025-06-21 17:40:19 -0500

    update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from utils.safety import validate_position, check_joint_limits
 
 # --- Configurable Constants ---
 ARM_PORT = 'COM5' # Make sure this is your correct COM port
-CAMERA_INDEX = 0  # Make sure this is your correct camera index for Logitech
+CAMERA_INDEX = 1  # Make sure this is your correct camera index for Logitech
 TEMP_IMAGE_PATH = "current_view.jpg"
 DEBUG_MODE = True
 RESOLUTION = (1280, 720)
